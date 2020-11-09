ab0b45fe8d0c473c72be69191400c213bce77256
hanur92 <rkdgksdjfwkd92@gmail.com>
2020-11-09 15:18:37 +0900

    ➕ make comp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Child1 from "./Child1";
+import Child2 from "./Child2";
+import Child3 from "./Child3";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Child1 props1_1="Child1-1"></Child1>
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Child3.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Child3 = (props3) => {
+  return <div>is Child3's FirstProps</div>;
+};
+
+export default Child3;
