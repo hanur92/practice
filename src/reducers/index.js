c7607f4354427c1934fed3e03d359d9119d4020f
hanur92 <rkdgksdjfwkd92@gmail.com>
2021-02-02 18:30:07 +0900

    🔧indent

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -30,6 +30,75 @@
 // */
 
 // export default reducers;
+////////////////////////////////////////////////////////////
+//setColor구현해보기 (타겟인덱스는 3)
+//타겟인덱스 전으로 스프레드 후 슬라이스
+//타겟인덱스에 접근해서 color의 밸류를 action.color로 변경
+//타겟인덱스 후로 스프레드 후 슬라이스
+// console.log({
+//   counters: [
+//     ...counters.slice(0, 3),
+//     { ...counters[3], color: "action.color" },
+//     ...counters.slice(4, counters.length),
+//   ],
+// });
+
+//decrement 구현해보기 (타겟인덱스는 3)
+//타겟인덱스 전으로 스프레드 후 슬라이스
+//타겟인덱스 추출 후 -1
+//타겟인덱스 후로 스프레드 후 슬라이스
+
+// console.log({
+//   counters: [
+//     ...counters.slice(0, 3),
+//     { ...counters[3], number: counters[3].number - 1 },
+//     ...counters.slice(4, counters.length - 1),
+//   ],
+// });
+// const { counters } = state;
+// console.log({
+//   //action.index = 5
+//   counters: [
+//     ...counters.slice(0, 5), //선택인덱스 직전까지 슬라이스
+//     { ...counters[5], number: counters[5].number + 1 },
+//     //
+//     // ...counters.slice(6, counters.length),
+//   ],
+// });
+// console.log({
+//   counters: [
+//     ...counters.slice(0, 5),
+//     { ...counters[5], number: counters[5].number + 1 },
+//   ],
+// });
+// console.log("0", {});
+// console.log("1", { counters });
+// console.log("2", { counters: counters });
+// console.log("3", { counters: [...counters] });
+// console.log("4", { counters: [...counters.slice(0, 5)] });
+// console.log("5", {
+//   counters: [...counters.slice(0, 5), { ...counters[5] }],
+// });
+// console.log("6", {
+//   counters: [
+//     ...counters.slice(0, 5),
+//     { ...counters[5], number: counters[5].number + 1 },
+//   ],
+// });
+// console.log({
+//   //action.index = 3
+//   counters: [
+//     ...counters.slice(0, 3), //선택인덱스 직전까지 자름
+//     { ...counters[3], number: counters[3].number + 1 },
+//     //목표인덱스를 잘라주고 숫자 1을 증가시켜줌
+//     ...counters.slice(4, counters.length),
+//     //목표인덱스 이후부터 counters배열의 끝(counters.length)까지 잘라줌
+//   ],
+// });
+
+//             {counters: [...counters.slice(0, action.index),{...counters[action.index],
+// number: counters[action.index].number + 1,},...counters.slice(action.index + 1, counters.length)],
+// };
 
 import * as types from "../actions/ActionTypes";
 import { Map, List } from "immutable";
@@ -45,75 +114,6 @@ const initialState = Map({
 
 function counter(state = initialState, action) {
   const counters = state.get("counters");
-
-  //setColor구현해보기 (타겟인덱스는 3)
-  //타겟인덱스 전으로 스프레드 후 슬라이스
-  //타겟인덱스에 접근해서 color의 밸류를 action.color로 변경
-  //타겟인덱스 후로 스프레드 후 슬라이스
-  // console.log({
-  //   counters: [
-  //     ...counters.slice(0, 3),
-  //     { ...counters[3], color: "action.color" },
-  //     ...counters.slice(4, counters.length),
-  //   ],
-  // });
-
-  //decrement 구현해보기 (타겟인덱스는 3)
-  //타겟인덱스 전으로 스프레드 후 슬라이스
-  //타겟인덱스 추출 후 -1
-  //타겟인덱스 후로 스프레드 후 슬라이스
-
-  // console.log({
-  //   counters: [
-  //     ...counters.slice(0, 3),
-  //     { ...counters[3], number: counters[3].number - 1 },
-  //     ...counters.slice(4, counters.length - 1),
-  //   ],
-  // });
-  // const { counters } = state;
-  // console.log({
-  //   //action.index = 5
-  //   counters: [
-  //     ...counters.slice(0, 5), //선택인덱스 직전까지 슬라이스
-  //     { ...counters[5], number: counters[5].number + 1 },
-  //     //
-  //     // ...counters.slice(6, counters.length),
-  //   ],
-  // });
-  // console.log({
-  //   counters: [
-  //     ...counters.slice(0, 5),
-  //     { ...counters[5], number: counters[5].number + 1 },
-  //   ],
-  // });
-  // console.log("0", {});
-  // console.log("1", { counters });
-  // console.log("2", { counters: counters });
-  // console.log("3", { counters: [...counters] });
-  // console.log("4", { counters: [...counters.slice(0, 5)] });
-  // console.log("5", {
-  //   counters: [...counters.slice(0, 5), { ...counters[5] }],
-  // });
-  // console.log("6", {
-  //   counters: [
-  //     ...counters.slice(0, 5),
-  //     { ...counters[5], number: counters[5].number + 1 },
-  //   ],
-  // });
-  // console.log({
-  //   //action.index = 3
-  //   counters: [
-  //     ...counters.slice(0, 3), //선택인덱스 직전까지 자름
-  //     { ...counters[3], number: counters[3].number + 1 },
-  //     //목표인덱스를 잘라주고 숫자 1을 증가시켜줌
-  //     ...counters.slice(4, counters.length),
-  //     //목표인덱스 이후부터 counters배열의 끝(counters.length)까지 잘라줌
-  //   ],
-  // });
-
-  //             {counters: [...counters.slice(0, action.index),{...counters[action.index],
-  // number: counters[action.index].number + 1,},...counters.slice(action.index + 1, counters.length)],
-  // };
 
   switch (action.type) {
     case types.CREATE:
